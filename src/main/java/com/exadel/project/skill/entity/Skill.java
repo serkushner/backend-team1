3bd78882b2264e5bf9b4e561d32002bb5b17f022
Aliaksei Kaliada <Aliaksei.Kaliada@gmail.com>
2021-03-30 21:51:19 +0300

    fix generation type

--- src/main/java/com/exadel/project/skill/entity/Skill.java
+++ src/main/java/com/exadel/project/skill/entity/Skill.java
@@ -15,7 +15,7 @@ public class Skill {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
