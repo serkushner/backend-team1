02bc73fea90d04fc129d4fcd163c394f0defb04c
learningGuru <nazar.furdychka50@gmail.com>
2021-03-30 22:58:02 +0200

    created Internship and InternshipType entity what need to be reviewed: not blank(null), date, packages,

---
+++ src/main/java/com/exadel/internship_type/entity/InternshipType.java
@@ -0,0 +1,29 @@
+package com.exadel.internship_type.entity;
+
+import com.exadel.project.internship.entity.Internship;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "internship_type")
+@Setter
+@Getter
+public class InternshipType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "type")
+    private String type;
+
+    @OneToMany(mappedBy="internship_type")
+    private List<Internship> comments = new ArrayList<>();
+}
---
+++ src/main/java/com/exadel/project/internship/entity/Internship.java
@@ -0,0 +1,62 @@
+package com.exadel.project.internship.entity;
+
+import com.exadel.internship_type.entity.InternshipType;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "internship")
+@Setter
+@Getter
+public class Internship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "Title should not be empty")
+    @Column(name = "title")
+    private String title;
+
+    @NotNull
+    @Column(name = "start_internship")
+    private java.sql.Date start_internship;
+
+    @NotNull
+    @Column(name = "end_internship")
+    private java.sql.Date end_internship;
+
+    @NotNull
+    @Column(name = "start_request")
+    private java.sql.Date start_request;
+
+    @NotNull
+    @Column(name = "end_request")
+    private java.sql.Date end_request;
+
+    @NotBlank(message = "Description should not be empty")
+    @Column(name = "description")
+    private String description;
+
+    @NotBlank(message = "Additional info should not be empty")
+    @Column(name = "additional_info")
+    private String additional_info;
+
+    @NotNull
+    @Column(name = "image")
+    private String image;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "format")
+    private Format format;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private InternshipType internship_type;
+
+
+}
