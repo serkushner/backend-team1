574454b44de75f509a08570dec79ae3201baae24
Sergei Shpakovski <shpaser2@yandex.ru>
2021-04-09 18:26:16 +0300

    [BE-23] work in process on the trainee entity file

--- src/main/java/com/exadel/project/trainee/entity/Trainee.java
+++ src/main/java/com/exadel/project/trainee/entity/Trainee.java
@@ -1,8 +1,11 @@
 package com.exadel.project.trainee.entity;
 
+import com.exadel.project.administrator.entity.Administrator;
+import com.exadel.project.internship.entity.Country;
 import lombok.Data;
-
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -13,5 +16,41 @@ public class Trainee {
     @Column(name = "id")
     private Long id;
 
+    @NotBlank(message = "Name of trainee should not be empty")
+    @Column(name = "name")
+    private String name;
 
+    @NotBlank(message = "Surname of trainee should not be empty")
+    @Column(name = "surname")
+    private String surname;
+
+    @NotBlank(message = "Email of trainee should not be empty")
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @NotBlank(message = "Phone number of trainee should not be empty")
+    @Column(name = "phone")
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id")
+    private TraineeStatus status;
+
+    @Column(name = "linkedin")
+    private String linkedInLink;
+
+    @Column(name = "skype")
+    private String skype;
+
+    @OneToOne
+    @JoinColumn()
+    private Administrator administrator;
+
+    @NotNull
+    @Column(name = "recipient")
+    private Boolean recipient;
 }
