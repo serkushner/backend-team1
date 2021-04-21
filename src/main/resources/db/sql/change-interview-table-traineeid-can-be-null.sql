348275b9bbb8a39907a7746962060ab1e17167b2
Sergei Shpakovski <shpaser2@yandex.ru>
2021-04-21 18:24:55 +0300

    [BE-60_64_65_66_67] preconditions added, sql not null to null fixed

--- src/main/resources/db/sql/change-interview-table-traineeid-can-be-null.sql
+++ src/main/resources/db/sql/change-interview-table-traineeid-can-be-null.sql
@@ -2,14 +2,5 @@
 
 -- 20210421.[BE-64]-change-interview-traineeid-can-be-null
 
-
 ALTER TABLE interview
-    DROP FOREIGN KEY trainee_interview @@
-
-ALTER TABLE interview
-    CHANGE COLUMN trainee_id trainee_id BIGINT (20) NULL @@
-
-ALTER TABLE interview
-    ADD CONSTRAINT trainee_interview
-        FOREIGN KEY (trainee_id)
-            REFERENCES trainee(id) @@
+    MODIFY COLUMN trainee_id BIGINT (20) NULL
