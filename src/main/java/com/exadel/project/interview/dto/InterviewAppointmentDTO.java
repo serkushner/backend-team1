7dea9d704ac56ef193195f7b0e56a11fe428fb9b
learningGuru <nazar.furdychka50@gmail.com>
2021-05-04 12:58:18 +0300

    [BE-73] little change

--- src/main/java/com/exadel/project/interview/dto/InterviewAppointmentDTO.java
+++ src/main/java/com/exadel/project/interview/dto/InterviewAppointmentDTO.java
@@ -1,14 +1,11 @@
 package com.exadel.project.interview.dto;
 
-import com.exadel.project.internship.dto.InternshipDTO;
-import com.exadel.project.interviewer.dto.InterviewerDTO;
-import com.exadel.project.trainee.dto.TraineeDTO;
 import lombok.Data;
 
 @Data
 public class InterviewAppointmentDTO {
-    private InterviewTimeDTO interviewTime;
     private Long traineeId;
     private Long internshipId;
     private Long interviewerId;
+    private InterviewTimeDTO interviewTime;
 }
