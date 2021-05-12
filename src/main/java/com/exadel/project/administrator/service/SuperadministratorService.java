ec891716d97307908c2176abcc3bd3fd789631f3
Sergei Fadin <nidaff.s@gmail.com>
2021-05-12 10:04:18 +0300

    no message

--- src/main/java/com/exadel/project/administrator/service/SuperadministratorService.java
+++ src/main/java/com/exadel/project/administrator/service/SuperadministratorService.java
@@ -22,7 +22,6 @@ public class SuperadministratorService {
 
     public TraineeToAdminDTO changeTraineeStatus(Long id, Boolean isApproved) {
         AdditionalInfo additionalInfo = additionalInfoRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-        //additionalInfo.setTraineeStatus(TraineeStatus.RECRUITER_INTERVIEW_REJECTED);
         if (!isApproved) {
             additionalInfo.setTraineeStatus(TraineeStatus.REJECTED);
         } else {
--- src/main/java/com/exadel/project/trainee/service/TraineeStatusService.java
+++ src/main/java/com/exadel/project/trainee/service/TraineeStatusService.java
@@ -1,6 +1,11 @@
 package com.exadel.project.trainee.service;
 
+import com.exadel.project.common.exception.EntityNotFoundException;
 import com.exadel.project.trainee.dto.TraineeToAdminDTO;
+import com.exadel.project.trainee.entity.AdditionalInfo;
+import com.exadel.project.trainee.entity.TraineeStatus;
+import com.exadel.project.trainee.mapper.AdditionalInfoMapper;
+import com.exadel.project.trainee.repository.AdditionalInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +13,28 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TraineeStatusService {
 
-    public TraineeToAdminDTO changeTraineeStatusToRejected(Long id) {
+    private final AdditionalInfoRepository additionalInfoRepository;
+    private final AdditionalInfoMapper additionalInfoMapper;
 
+    public TraineeToAdminDTO changeTraineeStatusToRejected(Long id) {
+        AdditionalInfo additionalInfo = additionalInfoRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        additionalInfo.setTraineeStatus(TraineeStatus.REJECTED);
+        additionalInfoRepository.save(additionalInfo);
+        return additionalInfoMapper.entityToDto(additionalInfo);
+    }
+
+    public TraineeToAdminDTO changeTraineeStatus(Long id) {
+        AdditionalInfo additionalInfo = additionalInfoRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        TraineeStatus status = additionalInfo.getTraineeStatus();
+        switch (status) {
+            case
+        }
+
+
+
+
+        additionalInfo.setTraineeStatus(TraineeStatus.REJECTED);
+        additionalInfoRepository.save(additionalInfo);
+        return additionalInfoMapper.entityToDto(additionalInfo);
     }
 }
