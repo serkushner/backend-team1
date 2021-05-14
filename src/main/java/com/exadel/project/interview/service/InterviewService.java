f55b1a1964fa552d3a3e37fb82e5f7556d5e5842
Aliaksei Kaliada <70597105+AlKaliada@users.noreply.github.com>
2021-05-14 11:36:23 +0300

    Be 76 sending notifications to the trainee and interviewer about the appointment of an interview (#53)
    * add interview created event logic
    
    * add trainee changing statuses
    
    * add trainee changing statuses
    
    * add trainee changing statuses
    
    * fix remarks
    
    Co-authored-by: Aliaksei Kaliada <Aliaksei.Kaliada@gmail.com>

--- src/main/java/com/exadel/project/interview/service/InterviewService.java
+++ src/main/java/com/exadel/project/interview/service/InterviewService.java
@@ -7,16 +7,25 @@ import com.exadel.project.common.service.rsql.RsqlSpecification;
 import com.exadel.project.internship.service.InternshipService;
 import com.exadel.project.interview.dto.InterviewAppointmentDTO;
 import com.exadel.project.interview.dto.InterviewDTO;
+import com.exadel.project.interview.dto.InterviewFormDTO;
 import com.exadel.project.interview.entity.Interview;
+import com.exadel.project.interview.event.InterviewCreatedEvent;
+import com.exadel.project.interview.mapper.InterviewFormMapper;
 import com.exadel.project.interview.mapper.InterviewMapper;
 import com.exadel.project.interview.repository.InterviewRepository;
 import com.exadel.project.interviewer.entity.Interviewer;
 import com.exadel.project.interviewer.service.InterviewerService;
+import com.exadel.project.trainee.entity.AdditionalInfo;
+import com.exadel.project.trainee.entity.EnglishLevel;
+import com.exadel.project.trainee.entity.TraineeStatus;
+import com.exadel.project.trainee.repository.AdditionalInfoRepository;
 import com.exadel.project.trainee.service.TraineeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,25 +43,38 @@ public class InterviewService extends BaseService<Interview, InterviewRepository
     private final InternshipService internshipService;
     private final InterviewerService interviewerService;
     private final InterviewTimeService interviewTimeService;
+    private final ApplicationEventPublisher applicationEventPublisher;
+    private final InterviewFormMapper interviewFormMapper;
+    private final AdditionalInfoRepository additionalInfoRepository;
+    private final JwtInterviewService jwtInterviewService;
 
     @Override
     public RsqlSpecification getRsqlSpecification() {
         throw new UnsupportedOperationException();
     }
 
+    @Transactional
     public InterviewAppointmentDTO addInterview(InterviewAppointmentDTO interviewAppointmentDTO) throws EntityAlreadyExistsException {
         Interview interview = new Interview();
         interview.setInternship(internshipService.getEntityById(interviewAppointmentDTO.getInternshipId()));
         interview.setTrainee(traineeService.getEntityById(interviewAppointmentDTO.getTraineeId()));
         interview.setInterviewTime(interviewTimeService.getEntityById(interviewAppointmentDTO.getInterviewTimeId()));
-
         Interviewer interviewer = interviewerService.getEntityById(interviewAppointmentDTO.getInterviewerId());
         interviewerService.deleteInterview(interviewer, interviewTimeService.getEntityById(interviewAppointmentDTO.getInterviewTimeId()));
         interview.setInterviewer(interviewer);
-
         interviewRepository.save(interview);
-        //TODO send emails
+        changeTraineeStatusAfterAddInterview(interview);
+        InterviewCreatedEvent interviewCreatedEvent = new InterviewCreatedEvent(this, interview);
+        applicationEventPublisher.publishEvent(interviewCreatedEvent);
         return interviewAppointmentDTO;
+    }
+
+    @Transactional
+    public void changeTraineeStatusAfterAddInterview(Interview interview){
+        AdditionalInfo additionalInfo = additionalInfoRepository.findAdditionalInfoByInternshipAndTrainee(interview.getInternship(), interview.getTrainee());
+        TraineeStatus traineeStatus = TraineeStatus.getNextStatus(additionalInfo.getTraineeStatus(), true);
+        additionalInfo.setTraineeStatus(traineeStatus);
+        additionalInfoRepository.save(additionalInfo);
     }
 
     public InterviewDTO getInterviewById(Long id) throws EntityNotFoundException {
@@ -90,5 +112,28 @@ public class InterviewService extends BaseService<Interview, InterviewRepository
                 .stream()
                 .map(interviewMapper::entityToDto)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public InterviewFormDTO getInterviewFormInfoByToken(String encryptedInterviewId){
+        jwtInterviewService.validateToken(encryptedInterviewId);
+        Long interviewId = Long.valueOf(jwtInterviewService.getIdFromToken(encryptedInterviewId));
+        Interview interview = interviewRepository.findById(interviewId).orElseThrow(EntityNotFoundException::new);
+        AdditionalInfo additionalInfo = additionalInfoRepository.findAdditionalInfoByInternshipAndTrainee(interview.getInternship(), interview.getTrainee());
+        return interviewFormMapper.entityToDto(interview, additionalInfo);
+    }
+
+    @Transactional
+    public InterviewFormDTO updateInterviewForm(InterviewFormDTO dto){
+        Interview interview = interviewRepository.findById(dto.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        AdditionalInfo additionalInfo = additionalInfoRepository.findAdditionalInfoByInternshipAndTrainee(interview.getInternship(), interview.getTrainee());
+        additionalInfo.setEnglish(EnglishLevel.valueOf(dto.getEnglish().toUpperCase()));
+        TraineeStatus status = TraineeStatus.getNextStatus(additionalInfo.getTraineeStatus(), dto.getInterviewerDecision());
+        additionalInfo.setTraineeStatus(status);
+        additionalInfoRepository.save(additionalInfo);
+        interview.setName(dto.getSubscription());
+        interviewRepository.save(interview);
+        return interviewFormMapper.entityToDto(interview, additionalInfo);
     }
 }
--- src/main/java/com/exadel/project/trainee/entity/TraineeStatus.java
+++ src/main/java/com/exadel/project/trainee/entity/TraineeStatus.java
@@ -1,15 +1,31 @@
 package com.exadel.project.trainee.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum TraineeStatus {
-    EMAIL_NOT_CONFIRM,
-    REGISTERED,
-    RECRUITER_INTERVIEW_PENDING,
-    RECRUITER_INTERVIEW_REJECTED,
-    RECRUITER_INTERVIEW_ACCEPTED,
-    RECRUITER_INTERVIEW_PASSED,
-    TECHNICAL_INTERVIEW_PENDING,
-    TECHNICAL_INTERVIEW_REJECTED,
-    TECHNICAL_INTERVIEW_ACCEPTED,
-    REJECTED,
-    ACCEPTED
+    EMAIL_NOT_CONFIRM("registered", null),
+    REGISTERED("recruiter_interview_pending", null),
+    RECRUITER_INTERVIEW_PENDING("recruiter_interview_accepted", "recruiter_interview_rejected"),
+    RECRUITER_INTERVIEW_REJECTED("recruiter_interview_passed", "rejected"),
+    RECRUITER_INTERVIEW_ACCEPTED("recruiter_interview_passed", "rejected"),
+    RECRUITER_INTERVIEW_PASSED("technical_interview_pending", null),
+    TECHNICAL_INTERVIEW_PENDING("technical_interview_accepted", "technical_interview_rejected"),
+    TECHNICAL_INTERVIEW_REJECTED("accepted", "rejected"),
+    TECHNICAL_INTERVIEW_ACCEPTED("accepted", "rejected"),
+    REJECTED(null, null),
+    ACCEPTED(null, null);
+
+    TraineeStatus(String nextPositive, String nextNegative) {
+        this.nextPositive = nextPositive;
+        this.nextNegative = nextNegative;
+    }
+
+    private final String nextPositive;
+    private final String nextNegative;
+
+    public static TraineeStatus getNextStatus(TraineeStatus currentStatus, Boolean positive){
+        String nextStatus = positive ? currentStatus.nextPositive : currentStatus.nextNegative;
+        return TraineeStatus.valueOf(nextStatus.toUpperCase());
+    }
 }
